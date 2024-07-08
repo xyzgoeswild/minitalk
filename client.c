11c69a5de3544e44c2698f458a1fe1b1955ee88d
amuhsen- <amuhsen-@c4r6p7.1337.ma>
2024-07-08 03:32:48 +0100

    Fixed norm

--- client.c
+++ client.c
@@ -6,61 +6,59 @@
 /*   By: amuhsen- <amuhsen-@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:37:00 by amuhsen-          #+#    #+#             */
-/*   Updated: 2024/06/28 23:35:48 by amuhsen-         ###   ########.fr       */
+/*   Updated: 2024/07/08 03:17:34 by amuhsen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <signal.h>
-#include <unistd.h>
 
-void send_msg(char c, int pid)
+void	send_msg(char c, int pid)
 {
-    int i;
+	int	i;
 
-    i = 8;
-    while (i > 0)
-    {
-        if (c & 1)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        c >>= 1;
-        usleep(300);
-        i--;
-    }
+	i = 8;
+	while (i > 0)
+	{
+		if (c & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		c >>= 1;
+		usleep(300);
+		i--;
+	}
 }
 
-int valid(int ac, char **av)
+int	valid(int ac, char **av)
 {
-    if (ac != 3)
-    {
-        write(1, RED"Invalid: ./client [PID] [Message]\n", 40);
-        return (0);
-    }
-    else if (ft_atoi(av[1]) <= 0)
-    {
-        write(1, RED"Invalid: Check PID!\n", 26);
-        return (0);
-    }
-    else
-        return (1);
+	if (ac != 3)
+	{
+		write(1, RED"Invalid: ./client [PID] [Message]\n", 40);
+		return (0);
+	}
+	else if (ft_atoi(av[1]) <= 0)
+	{
+		write(1, RED"Invalid: Check PID!\n", 26);
+		return (0);
+	}
+	else
+		return (1);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    int pid;
-    int i;
+	int	pid;
+	int	i;
 
-    if (valid(ac, av) == 0)
-        return (0);
-    pid = ft_atoi(av[1]);
-    i = 0;
-    while (av[2][i])
-    {
-        send_msg(av[2][i], pid);
-        i++;
-    }
-    send_msg('\0', pid);
-    return (0);
+	if (valid(ac, av) == 0)
+		return (0);
+	pid = ft_atoi(av[1]);
+	i = 0;
+	while (av[2][i])
+	{
+		send_msg(av[2][i], pid);
+		i++;
+	}
+	send_msg('\0', pid);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,61 +6,59 @@
 /*   By: amuhsen- <amuhsen-@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:37:02 by amuhsen-          #+#    #+#             */
-/*   Updated: 2024/06/28 23:35:17 by amuhsen-         ###   ########.fr       */
+/*   Updated: 2024/07/08 03:29:09 by amuhsen-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-#include <unistd.h>
 
-void    handler(int sig)
+void	handler(int sig)
 {
-    static unsigned char c;
-    static int  i;
+	static unsigned char	c;
+	static int				i;
 
-    if (sig == SIGUSR1)
-        c = c | (1<<i);
-    else if (sig == SIGUSR2)
-        c = c | (0<<i);
-    i++;
-    if (i == 8)
-    {
-        if (c == '\0')
-        {
-            ft_putchar_fd('\n', 1);
-            c = 0;
-            i = 0;
-        }
-        else
-        {
-            ft_putchar_fd(c, 1);
-            c = 0;
-            i = 0;
-        }
-    }
+	if (sig == SIGUSR1)
+		c = c | (1 << i);
+	else if (sig == SIGUSR2)
+		c = c | (0 << i);
+	i++;
+	if (i == 8)
+	{
+		if (c == '\0')
+		{
+			ft_putchar_fd('\n', 1);
+			c = 0;
+			i = 0;
+		}
+		else
+		{
+			ft_putchar_fd(c, 1);
+			c = 0;
+			i = 0;
+		}
+	}
 }
 
-void    h_sigint()
+void	display_sigint(int sig)
 {
-    write(1, RED"\nProcess Terminated [2]!\n", 31);
-    exit(0);
+	(void) sig;
+	write(1, RED"\nProcess Terminated [2]!\n", 31);
+	exit(0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    (void) **av;
-    if (ac != 1)
-        return (0);
-
-    write(1, GREEN"Server PID: ", 18);
-    ft_putnbr_fd(getpid(), 1);
-    write(1, YELLOW"\nAwaiting Message...\n", 27);
-    write(1, RESET, 4);
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
-    signal(SIGINT, h_sigint);
-    while (1) {
-        ;
-    }
-    return (0);
+	(void) **av;
+	if (ac != 1)
+		return (0);
+	write(1, GREEN"Server PID: ", 18);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, YELLOW"\nAwaiting Message...\n", 27);
+	write(1, RESET, 4);
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	signal(SIGINT, display_sigint);
+	while (1)
+		;
+	return (0);
 }
